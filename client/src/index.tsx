8182cfaa9c26f434ce2b9d1b1c0bdd163a53df49
Thomas Grininger <thomas.grininger@ringodev.com>
2025-01-08 10:41:05 +0100

    upgrade react version

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
-// import { LocalizationProvider } from "@mui/lab";
 import App from "./App";
 
-ReactDOM.render(
+// After
+import { createRoot } from "react-dom/client";
+
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root"),
 );
